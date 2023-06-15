f60ee22e74cab039581a67a104ff9457cf3d4949
Ashwini Kumar Pandey <itsashwani001@gmail.com>
2023-06-15 23:26:39 +0530

    pages and menu routes

--- server.js
+++ server.js
@@ -227,3 +227,247 @@ app.get('/user_logs/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+// Create a new page
+app.post('/pages', async (req, res) => {
+  try {
+    const {
+      slug,
+      title,
+      description,
+      content,
+      tags,
+      meta,
+      show_in_menu,
+      status,
+    } = req.body;
+
+    const query = `INSERT INTO pages
+      (slug, title, description, content, tags, meta, show_in_menu, status)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING *`;
+
+    const values = [
+      slug,
+      title,
+      description,
+      content,
+      tags,
+      meta,
+      show_in_menu,
+      status,
+    ];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Get all pages
+app.get('/pages', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM pages';
+    const client = await pool.connect();
+    const result = await client.query(query);
+    client.release();
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Get a specific page by ID
+app.get('/pages/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const query = 'SELECT * FROM pages WHERE id = $1';
+    const values = [id];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Page not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Update a page by ID
+app.put('/pages/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      slug,
+      title,
+      description,
+      content,
+      tags,
+      meta,
+      show_in_menu,
+      status,
+    } = req.body;
+
+    const query = `UPDATE pages
+      SET slug = $1, title = $2, description = $3, content = $4, tags = $5, meta = $6, show_in_menu = $7, status = $8, updated_at = NOW()
+      WHERE id = $9
+      RETURNING *`;
+       const values = [
+      slug,
+      title,
+      description,
+      content,
+      tags,
+      meta,
+      show_in_menu,
+      status,
+      id,
+    ];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Page not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Delete a page by ID
+app.delete('/pages/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const query = 'DELETE FROM pages WHERE id = $1';
+    const values = [id];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'Page not found' });
+    }
+
+    res.json({ message: 'Page deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Create a new menu item
+app.post('/menu', async (req, res) => {
+  try {
+    const { page_id, position } = req.body;
+
+    const query = `INSERT INTO menu (page_id, position) VALUES ($1, $2) RETURNING *`;
+    const values = [page_id, position];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Get all menu items
+app.get('/menu', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM menu';
+    const client = await pool.connect();
+    const result = await client.query(query);
+    client.release();
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Get a specific menu item by ID
+app.get('/menu/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const query = 'SELECT * FROM menu WHERE id = $1';
+    const values = [id];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Menu item not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// Update a menu item by ID
+app.put('/menu/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { page_id, position } = req.body;
+
+    const query = 'UPDATE menu SET page_id = $1, position = $2 WHERE id = $3 RETURNING *';
+    const values = [page_id, position, id];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Menu item not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Delete a menu item by ID
+app.delete('/menu/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const query = 'DELETE FROM menu WHERE id = $1';
+    const values = [id];
+
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+     if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'Menu item not found' });
+    }
+
+    res.json({ message: 'Menu item deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
