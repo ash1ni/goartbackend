86c2d58846bd40c7f1a37880de7e720856194e9c
Ashwini Kumar Pandey <ashwini.pandey@weverve.com>
2023-07-01 19:09:44 +0530

    collection-media routes to perform operations

--- server.js
+++ server.js
@@ -1506,3 +1506,80 @@ app.delete('/artist-media/:id', async (req, res) => {
   }
 });
 
+// GET all collection_media records
+app.get('/collection_media', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM collection_media');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// GET a specific collection_media record by ID
+app.get('/collection_media/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM collection_media WHERE id = $1', [id]);
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Collection media not found' });
+    } else {
+      res.json(result.rows[0]);
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// POST a new collection_media record
+app.post('/collection_media', async (req, res) => {
+  const { position, collection_id, media_id } = req.body;
+  try {
+    const result = await pool.query(
+      'INSERT INTO collection_media (position, collection_id, media_id) VALUES ($1, $2, $3) RETURNING *',
+      [position, collection_id, media_id]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// PUT to update a specific collection_media record by ID
+app.put('/collection_media/:id', async (req, res) => {
+  const { id } = req.params;
+  const { position, collection_id, media_id } = req.body;
+  try {
+    const result = await pool.query(
+      'UPDATE collection_media SET position = $1, collection_id = $2, media_id = $3 WHERE id = $4 RETURNING *',
+      [position, collection_id, media_id, id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Collection media not found' });
+    } else {
+      res.json(result.rows[0]);
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// DELETE a collection_media record by ID
+app.delete('/collection_media/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('DELETE FROM collection_media WHERE id = $1 RETURNING *', [id]);
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Collection media not found' });
+    } else {
+      res.json({ message: 'Collection media deleted successfully' });
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
